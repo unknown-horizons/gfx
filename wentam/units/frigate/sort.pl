b7455af331a5684cadd2291604e3312b0149f243
wentam <wentam@wentam.(none)>
2011-07-17 18:55:09 -0400

    added fixed frigate renderings

--- wentam/units/frigate/sort.pl
+++ wentam/units/frigate/sort.pl
@@ -4,7 +4,7 @@
 # [folder_left], [folder_right], [angle(folder name)]
 
 
-my @range = (10,94);
+my @range = (11,94);
 
 my $folderleft = @ARGV[0];
 my $folderright = @ARGV[1];
@@ -12,10 +12,10 @@ my $angle = @ARGV[2];
 
 for (my $i = @range[0]; $i <= @range[1]; $i++) {
     if ($i <= 52) {
-	my $dest = $i-10;
+	my $dest = $i-@range[0];
 	qx(cp raw/00$i.png $folderleft/$angle/$dest.png);
     } else {
-	my $dest = $i-52;
+	my $dest = $i-53;
 	qx(cp raw/00$i.png $folderright/$angle/$dest.png);
     }
 }
